533d2ea7554bd765f3990953c7f403d47e64ff64
yash <yashwanth.k@mooo.farm>
2023-08-14 18:12:44 +0530

    minor changes

--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -1,7 +1,13 @@
-import { Fragment, memo } from "react";
+import { Fragment, memo, useEffect } from "react";
 import "./homepage.styles.css";
 
 const HomePage = () => {
+  useEffect(() => {
+    document.body.style.backgroundColor = "#fbfbfb";
+    return () => {
+      document.body.style.backgroundColor = "#fbfbfb";
+    };
+  }, []);
   return (
     <Fragment>
       <div>welcome to home page</div>
