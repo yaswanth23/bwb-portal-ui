d7eca1e3a91d950c90524f128bf46d71ae5ff036
yash <yashwanth.k@mooo.farm>
2023-08-14 14:45:49 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,38 +1,38 @@
 import { Fragment, lazy, Suspense, useState } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 
-const Navigation = lazy(() =>
-  import("./routes/navigation/navigation.component")
-);
+// const Navigation = lazy(() =>
+//   import("./routes/navigation/navigation.component")
+// );
 const Home = lazy(() => import("./routes/home/home.component"));
 const LoginPage = lazy(() => import("./pages/login/loginpage.component"));
 
-const randomLoaderTexts = [
-  "The greatest wealth is health.",
-  "Happiness is the best medicine.",
-  "Wellness starts with you.",
-  "Healthcare is about caring, not just curing.",
-  "Wellness is a journey, not a destination.",
-  "Medicine is a science, and healing is an art.",
-];
+// const randomLoaderTexts = [
+//   "The greatest wealth is health.",
+//   "Happiness is the best medicine.",
+//   "Wellness starts with you.",
+//   "Healthcare is about caring, not just curing.",
+//   "Wellness is a journey, not a destination.",
+//   "Medicine is a science, and healing is an art.",
+// ];
 
 const App = () => {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const circleLoader = document.getElementById("preloader");
-  const randomIndex = Math.floor(Math.random() * randomLoaderTexts.length);
-  const randomLoaderText = randomLoaderTexts[randomIndex];
-  const textElement = document.getElementById("rnd-loader-text");
-  if (textElement) {
-    textElement.innerHTML = randomLoaderText;
-  }
+  // const circleLoader = document.getElementById("preloader");
+  // const randomIndex = Math.floor(Math.random() * randomLoaderTexts.length);
+  // const randomLoaderText = randomLoaderTexts[randomIndex];
+  // const textElement = document.getElementById("rnd-loader-text");
+  // if (textElement) {
+  //   textElement.innerHTML = randomLoaderText;
+  // }
 
-  if (circleLoader) {
-    setTimeout(() => {
-      circleLoader.style.display = "none";
-      setLoading(false);
-    }, 6000);
-  }
+  // if (circleLoader) {
+  //   setTimeout(() => {
+  //     circleLoader.style.display = "none";
+  //     setLoading(false);
+  //   }, 6000);
+  // }
 
   const handleLogin = () => {
     setIsLoggedIn(true);
--- src/pages/login/loginpage.component.js
+++ src/pages/login/loginpage.component.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Helmet } from "react-helmet-async";
+// import { Helmet } from "react-helmet-async";
 import { Link } from "react-router-dom";
 import "./login.styles.css";
 
