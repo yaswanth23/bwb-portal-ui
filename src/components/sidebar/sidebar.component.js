0959eb5540b25b196629ecb0427cf553f9df5d70
yash <yashwanth.k@mooo.farm>
2023-08-15 23:02:27 +0530

    dashboard changes

--- src/components/sidebar/sidebar.component.js
+++ src/components/sidebar/sidebar.component.js
@@ -1,8 +1,15 @@
-import { Fragment, memo, useEffect } from "react";
+import { Fragment, memo, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./sidebar.styles.css";
 
+import budget_b from "../../assets/icons/budget-blue.svg";
+import budget_w from "../../assets/icons/budget-white.svg";
+import home_b from "../../assets/icons/home-blue.svg";
+import home_w from "../../assets/icons/home-white.svg";
+
 const Sidebar = () => {
+  const [activeLinkIdx, setActiveLinkIdx] = useState(1);
+
   useEffect(() => {
     document.body.style.backgroundColor = "#fbfbfb";
     return () => {
@@ -18,7 +25,42 @@ const Sidebar = () => {
             <span className="sb-sub-logo-name">bharat</span>wellbeing
           </Link>
         </div>
-        <div>welcome to sidebar</div>
+        <nav className="sb-navigation">
+          <ul className="sb-nav-list">
+            <li className="sb-nav-item">
+              <Link
+                to="/"
+                onClick={() => setActiveLinkIdx(1)}
+                className={`sb-nav-link ${
+                  1 === activeLinkIdx ? "active" : null
+                }`}
+              >
+                <img
+                  src={1 === activeLinkIdx ? home_b : home_w}
+                  className="sb-nav-link-icon"
+                  alt="home-icon"
+                />
+                <span className="sb-nav-link-text">Home</span>
+              </Link>
+            </li>
+            <li className="sb-nav-item">
+              <Link
+                to="/"
+                onClick={() => setActiveLinkIdx(2)}
+                className={`sb-nav-link ${
+                  2 === activeLinkIdx ? "active" : null
+                }`}
+              >
+                <img
+                  src={2 === activeLinkIdx ? budget_b : budget_w}
+                  className="sb-nav-link-icon"
+                  alt="book-diagnostics-icon"
+                />
+                <span className="sb-nav-link-text">Book Diagnostics</span>
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </div>
     </Fragment>
   );
