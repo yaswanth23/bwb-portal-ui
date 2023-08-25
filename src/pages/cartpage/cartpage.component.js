dde46b31a3eec8d934bf0a34eb70b9793117ec9a
yash <yashwanth.k@mooo.farm>
2023-08-25 19:30:33 +0530

    added default cart page

--- src/pages/cartpage/cartpage.component.js
+++ src/pages/cartpage/cartpage.component.js
@@ -1,10 +1,33 @@
-import { memo } from "react";
+import { memo, useEffect } from "react";
 import "./cartpage.styles.css";
 
+import EmptyCartBanner from "../../assets/images/microscope.png";
+
 const CartPage = () => {
+  useEffect(() => {
+    const imageList = [EmptyCartBanner];
+    imageList.forEach((image) => {
+      const newImage = new Image();
+      newImage.src = image;
+      window[image] = newImage;
+    });
+  }, []);
   return (
     <>
-      <div>cart</div>
+      <div className="cp-empty-cart-page-container">
+        <div className="cp-empty-cart-main">
+          <div className="cp-empty-cart-img">
+            <img
+              className="cp-empty-cart-image-main"
+              alt="empty-cart"
+              src={EmptyCartBanner}
+            />
+          </div>
+          <div className="cp-empty-cart-img-label">
+            Looks like you have no items in your cart yet.
+          </div>
+        </div>
+      </div>
     </>
   );
 };
