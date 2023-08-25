7f6d64f35104ae9e94fe54a1ae99336280c4f9f9
yash <yashwanth.k@mooo.farm>
2023-08-25 13:35:24 +0530

    added patient details

--- src/pages/bookdiagnostics/bookdiagnostics.component.js
+++ src/pages/bookdiagnostics/bookdiagnostics.component.js
@@ -19,6 +19,11 @@ const BookDiagnosticsPage = () => {
   const [openDiagnosticDropdown, setOpenDiagnosticDropdown] = useState(false);
   const limit = 10;
 
+  const [fullName, setFullName] = useState("");
+  const [mobileNumber, setMobileNumber] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+
   useEffect(() => {
     if (inputPincodeValue !== "" && inputPincodeValue != 0) {
       fetch(
@@ -138,12 +143,116 @@ const BookDiagnosticsPage = () => {
     setInputDiagnosticValue(event.target.value);
   };
 
+  const handleFullNameChange = (event) => {
+    setFullName(event.target.value);
+  };
+
+  const handleMobileNumberChange = (event) => {
+    const newMobileNumber = +event.target.value;
+    if (!isNaN(newMobileNumber)) {
+      if (newMobileNumber.toString().length <= 10) {
+        setMobileNumber(newMobileNumber.toString());
+      }
+    }
+  };
+
+  const handleGenderChange = (event) => {
+    setGender(event.target.value);
+  };
+
+  const handleAgeChange = (event) => {
+    const newAge = +event.target.value;
+    if (!isNaN(newAge)) {
+      if (newAge.toString().length <= 2) {
+        setAge(newAge.toString());
+      }
+    }
+  };
+
   return (
     <>
       <div className="bdp-container">
         <div className="bdp-first-container">
-          <div className="bdp-first-book-dignostics-label">
-            <h3>Book Diagnostics</h3>
+          <div className="bdp-first-patient-details-container">
+            <div className="bdp-first-patient-details-label">
+              <h3>Patient Details</h3>
+            </div>
+            <div className="bdp-first-patient-details-form-main">
+              <div className="bdp-first-patient-details-f-inputs">
+                <div className="bdp-first-patient-details-f-name">
+                  <p className="bdp-p-name-label">Full Name</p>
+                  <input
+                    type="text"
+                    required
+                    name="fullName"
+                    value={fullName}
+                    onChange={handleFullNameChange}
+                    className="bdp-p-name-field"
+                    placeholder="Full Name"
+                    autoComplete="off"
+                  />
+                </div>
+                <div className="bdp-first-patient-details-f-mobile">
+                  <p className="bdp-p-mobile-label">Mobile Number</p>
+                  <input
+                    type="text"
+                    required
+                    name="mobileNumber"
+                    value={mobileNumber == 0 ? "" : mobileNumber}
+                    onChange={handleMobileNumberChange}
+                    className="bdp-p-mobile-field"
+                    placeholder="Mobile Number"
+                    autoComplete="off"
+                    maxLength="10"
+                  />
+                </div>
+              </div>
+              <div className="bdp-first-patient-details-s-inputs">
+                <div className="bdp-first-patient-details-s-age">
+                  <p className="bdp-p-age-label">Age</p>
+                  <input
+                    type="text"
+                    required
+                    name="age"
+                    value={age == 0 ? "" : age}
+                    onChange={handleAgeChange}
+                    className="bdp-p-age-field"
+                    placeholder="Age"
+                    autoComplete="off"
+                    maxLength="2"
+                  />
+                </div>
+                <div className="bdp-first-patient-details-s-gender">
+                  <p className="bdp-p-gender-label">Gender</p>
+                  <div className="bdp-p-gender-main">
+                    <div className="bdp-p-gender-sub">
+                      <input
+                        type="radio"
+                        required
+                        checked={gender === "Male"}
+                        onChange={handleGenderChange}
+                        className="bdp-p-gender-field"
+                        value="Male"
+                        name="gender"
+                      />
+                      <span>Male</span>
+                    </div>
+                    <div className="bdp-p-gender-sub">
+                      <input
+                        type="radio"
+                        required
+                        checked={gender === "Female"}
+                        onChange={handleGenderChange}
+                        className="bdp-p-gender-field"
+                        value="Female"
+                        name="gender"
+                      />
+                      <span>Female</span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div className="bdp-first-sub-one-container">
             <div className="bdp-search-container">
@@ -267,11 +376,6 @@ const BookDiagnosticsPage = () => {
               />
             </div>
           )}
-          <div className="bdp-first-patient-details-container">
-            <div className="bdp-first-patient-details-label">
-              <h3>Patient Details</h3>
-            </div>
-          </div>
         </div>
       </div>
     </>
